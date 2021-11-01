745694b93e52795df47f19917942580741a52041
Alex Garay <alex.garay94@gmail.com>
2021-11-01 16:52:54 -0700

    setup VRC websocket connection

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-require('dotenv').config()
+require('dotenv').config();
+const {client: WebSocketClient} = require('websocket');
+const client = new WebSocketClient();
 
-console.log(process.env)
+client.on("connect", (connection) => {
+    console.log('WebSocket Client Connected');
+    connection.on('error', function (error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function () {
+        console.log('echo-protocol Connection Closed');
+    });
+    connection.on('message', function (message) {
+
+    });
+})
+
+client.connect(
+    `wss://pipeline.vrchat.cloud/?authToken=${process.env.VRC_AUTH_TOKEN}`,
+    "echo-protocol",
+    null,
+    {
+        "User-Agent": "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion"
+    }
+);
