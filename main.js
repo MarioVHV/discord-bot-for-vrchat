3742d1d95acd1d1acf90096597b9dbea6d2d44f4
Alex Garay <alex.garay94@gmail.com>
2021-11-01 18:30:01 -0700

    onlineFriends array now adds or removes friend based on notification type

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import websocket from 'websocket'
+import axios from "axios";
 import vrchat from "vrchat";
+import moment from 'moment';
 
 dotenv.config();
 
@@ -17,17 +19,32 @@ const FriendsApi = new vrchat.FriendsApi(configuration);
 
 let onlineFriends = [];
 let friendId = {};
+
+axios.defaults.headers = {
+    'Content-Type': 'application/json',
+    Authorization: 'Bot NzEwNzAxODUwNjY3Nzc4MDc5.Xr4SrQ.DNE-JA8jMyjVGR9v8GfMsQivghc'
+}
+
 AuthenticationApi.getCurrentUser()
     .then(() => {
         FriendsApi.getFriends()
             .then(({ data: friends }) => {
                 friends.map((friend) => {
+                    friendId[friend.id] = friend.displayName;
                     if (friend.location.length > 0) {
-                        friendId[friend.id] = friend.displayName;
-                        onlineFriends.push(friend.displayName)
+                        onlineFriends.push(friend.displayName);
                     }
                 })
-                console.log(onlineFriends)
+                console.log(onlineFriends);
+                axios({
+                    method: 'patch',
+                    url: `https://discord.com/api/channels/${process.env.DISCORD_CHANNEL_ID}/messages/${process.env.DISCORD_MESSAGE_ID}`,
+                    data: {
+                        "content": `Currently Online (Last Updated: ${moment().format('LTS')}): ${onlineFriends.map((friend) => {
+                            return `<:6164lightgreensmalldot:904307577192607754>${friend}`
+                        })}`
+                    }
+                });
             })
     })
 
@@ -40,7 +57,27 @@ client.on("connect", (connection) => {
         console.log('echo-protocol Connection Closed');
     });
     connection.on('message', function (message) {
+        let parsed = JSON.parse(message.utf8Data);
+        let { type, content } = parsed;
+        content = JSON.parse(content);
 
+        if (type === "friend-offline") {
+            console.log(`${friendId[content.userId]} has gone offline.`);
+            onlineFriends = onlineFriends.filter((friend) => {
+                return friend !== `${friendId[content.userId]}`;
+            })
+            console.log(onlineFriends)
+        }
+
+        if (type === "friend-online" && content.user.location.length > 0) {
+            console.log(`${content.user.displayName} has gone online.`);
+            onlineFriends.push(content.user.displayName)
+            console.log(onlineFriends)
+        }
+
+        if (type === "friend-active") {
+            console.log(`${content.user.displayName} has gone active.`)
+        }
     });
 })
 
