58d8974d0e9aaf24c87b3fadd36de73fd960ed27
Alex Garay <alex.garay94@gmail.com>
2021-11-05 00:53:54 -0700

    change in logic for updating friends

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ const FriendsApi = new vrchat.FriendsApi(configuration);
 let onlineFriends = [];
 let friendId = {};
 
+const connectWebsocket = () => {
+    client.connect(
+        `wss://pipeline.vrchat.cloud/?authToken=${process.env.VRC_AUTH_TOKEN}`,
+        "echo-protocol",
+        null,
+        {
+            "User-Agent": "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion"
+        }
+    );
+}
+
 const updateDiscord = () => {
     axios({
         method: 'patch',
@@ -37,22 +48,24 @@ const updateDiscord = () => {
     });
 }
 
-AuthenticationApi.getCurrentUser()
-    .then(() => {
-        FriendsApi.getFriends()
-            .then(({ data: friends }) => {
-                onlineFriends = [];
-                friends.map((friend) => {
-                    friendId[friend.id] = friend.displayName;
-                    if (friend.location.length > 0 && friend.displayName !== "xero_chris" && friend.displayName !== "Alfafles c3a1" && friend.displayName !== "S̳l̳u̳g̳s̳V̳R̳" && friend.displayName !== "ShortyPebbles20" && friend.displayName !== "XenoRyoga" && friend.displayName !== "Spartan81000" && friend.displayName !== "Tayッ a421") {
-                        onlineFriends.push(friend.displayName);
-                    }
+const updateFriends = () => {
+    AuthenticationApi.getCurrentUser()
+        .then(() => {
+            FriendsApi.getFriends()
+                .then(({ data: friends }) => {
+                    onlineFriends = [];
+                    friends.map((friend) => {
+                        friendId[friend.id] = friend.displayName;
+                        if (friend.location.length > 0 && friend.displayName !== "AwesomePizza_01" && friend.displayName !== "Angel_wolfyvr" && friend.displayName !== "xero_chris" && friend.displayName !== "Alfafles c3a1" && friend.displayName !== "S̳l̳u̳g̳s̳V̳R̳" && friend.displayName !== "XenoRyoga" && friend.displayName !== "Spartan81000" && friend.displayName !== "Tayッ a421") {
+                            onlineFriends.push(friend.displayName);
+                        }
+                    })
+                    console.log(onlineFriends);
+                    updateDiscord();
                 })
-                console.log(onlineFriends);
-                updateDiscord();
-            })
-    })
-
+        })
+}
+updateFriends();
 client.on("connect", (connection) => {
     console.log('WebSocket Client Connected');
     connection.on('error', function (error) {
@@ -60,42 +73,42 @@ client.on("connect", (connection) => {
     });
     connection.on('close', function () {
         console.log('echo-protocol Connection Closed');
+        connectWebsocket()
     });
     connection.on('message', function (message) {
-        let parsed = JSON.parse(message.utf8Data);
-        let { type, content } = parsed;
-        content = JSON.parse(content);
-
-        if (type === "friend-offline") {
-            console.log(`${friendId[content.userId]} has gone offline.`);
-            onlineFriends = onlineFriends.filter((friend) => {
-                return friend !== `${friendId[content.userId]}`;
-            })
-            console.log(onlineFriends);
-            updateDiscord();
-        }
-
-        if (type === "friend-online") {
-            console.log(`${content.user.displayName} has gone online.`);
-            if (content.user.displayName !== "xero_chris" && content.user.displayName !== "Alfafles c3a1" && content.user.displayName !== "S̳l̳u̳g̳s̳V̳R̳" && content.user.displayName !== "ShortyPebbles20" && content.user.displayName !== "XenoRyoga" && content.user.displayName !== "Spartan81000" && content.user.displayName !== "Tayッ a421") {
-                onlineFriends.push(content.user.displayName)
+        if (message.type === 'utf8') {
+            let parsed = JSON.parse(message.utf8Data);
+            let { type, content } = parsed;
+            if (message.type === 'utf8') { 
+                content = JSON.parse(content);
             }
-            console.log(onlineFriends)
-            updateDiscord();
+            if (type === "friend-offline") {
+                // console.log(`${friendId[content.userId]} has gone offline.`);
+                // onlineFriends = onlineFriends.filter((friend) => {
+                //     return friend !== `${friendId[content.userId]}`;
+                // })
+                // console.log(onlineFriends);
+                updateFriends();
+                // updateDiscord();
+            }
+    
+            if (type === "friend-online") {
+                console.log(`${content.user.displayName} has gone online.`);
+                // if (onlineFriends.indexOf(content.user.displayName) === -1 && content.user.displayName !== "xero_chris" && content.user.displayName !== "Alfafles c3a1" && content.user.displayName !== "S̳l̳u̳g̳s̳V̳R̳" && content.user.displayName !== "ShortyPebbles20" && content.user.displayName !== "XenoRyoga" && content.user.displayName !== "Spartan81000" && content.user.displayName !== "Tayッ a421") {
+                //     onlineFriends.push(content.user.displayName)
+                // }
+                // console.log(onlineFriends)
+                updateFriends();
+                // updateDiscord();
+            }
+    
+            if (type === "friend-active") {
+                console.log(`${content.user.displayName} has gone active.`)
+            }
         }
 
-        if (type === "friend-active") {
-            console.log(`${content.user.displayName} has gone active.`)
-        }
     });
 })
 
 discord.login(process.env.DISCORD_BOT_TOKEN);
-client.connect(
-    `wss://pipeline.vrchat.cloud/?authToken=${process.env.VRC_AUTH_TOKEN}`,
-    "echo-protocol",
-    null,
-    {
-        "User-Agent": "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion"
-    }
-);
+connectWebsocket();
